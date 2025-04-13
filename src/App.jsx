756e406195d11f572af4423150a68cd73c5ad5c5
udeh ujunwa faustina <udehujunwa487@gmail.com>
2025-04-13 05:45:34 +0100

    Add Brand page component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import './index.css'
 import Hero from './components/Hero/Hero'
+import Brand from './components/Brand';
 const App = () => {
   return (
     <div>
      <Hero/>
+     <Brand />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
